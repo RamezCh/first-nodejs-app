b7a7f9f9027a223f5d356c00e016f2f3d4b2cf69
RamezCh <ramiz_chreide@outlook.com>
2024-09-10 16:50:30 +0300

    parsing req body

--- app.js
+++ app.js
@@ -14,7 +14,18 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === '/message' && req.method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+    req.on('data', chunk => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      // buffer is a global object in node.js
+      // it is our bus stop
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
     // 302 means redirection
     res.statusCode = 302;
     res.setHeader('Location', '/');
